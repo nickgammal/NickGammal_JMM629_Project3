dd60ceb180609be0d4ad4d1447ef695cfde12a16
nickgammal <gammalnick@gmail.com>
2025-05-06 19:31:20 -0400

    Update (Final)

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ console.log(stickperson);
 function growStickFigure(event) {
     newheight = event.target.value / 300 * 70;
     if(newheight) {stickperson.style.height = newheight + "vh";}
-    else {stickperson.style.height = 10 + "vh";}
+    else {stickperson.style.height = "70%";}
 }
